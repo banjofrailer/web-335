a6b5a54a5a078c91761b3c865a6536b5935de933
Sarah Kovar <sarahkovar@gmail.com>
2020-06-21 20:56:35 -0500

    week 8

---
+++ week-8/kovar_calculator.py
@@ -0,0 +1,33 @@
+# ============================================
+# Title:  kovar_calculator.py
+# Author: Sarah Kovar
+# Date:   19 June 2020
+# Modified by: Sarah Kovar
+# Description: Python add, subtract, divide functions.
+# ===========================================
+
+
+first_name = 'Sarah'
+
+last_name = 'Kovar'
+
+print(first_name + ' ' + last_name)
+
+
+
+#add function
+def add(number01, number02):
+    return number01 + number02
+
+#subtract function
+def subtract(number01, number02):
+    return number01 - number02
+
+#divide function
+def divide(number01, number02):
+    return number01 / number02
+
+#print output
+print(add(1, 2))
+print(subtract(4, 1))
+print(divide(8, 2))
---
+++ week-8/kovar_hello_world.py
@@ -0,0 +1,9 @@
+# ============================================
+# Title:  kovar_hello_world.py
+# Author: Sarah Kovar
+# Date:   18 June 2020
+# Modified by: Sarah Kovar
+# Description: Python exercise to print Hello World.
+# ===========================================
+
+print('Hello World')
